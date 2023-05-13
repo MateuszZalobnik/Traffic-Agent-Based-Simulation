d47fb9a3bf901a7efd7eecd3a906cc7b0f3f0a3e
Vaxjo <matiz12345678901@wp.pl>
2023-05-13 18:20:51 +0200

    first

---
+++ src/MoveModel.java
@@ -0,0 +1,153 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MoveModel {
+    private int numberOfCars = 0; // number of cars in one wave
+    private int numberOfWaves = 1; // all cars
+    private int wavesTime = 1; // all cars
+    private int MaxTimeReaction = 0; // max time reaction
+    private int MinTimeReaction = 0; // min time reaction
+    private int trafficLightsTime = 9; // traffic lights time change
+    private boolean reverse = false; // true = move right to left
+
+    private ArrayList<Car> AllCars; // all cars
+    private static int positionYLeftToRight = 32; // y position
+    private static int positionYRightToLeft = 5; // y position
+    private Car[] board;
+    private ArrayList<Car> listOfCars;
+    private TrafficLight trafficLight;
+    private int timeStep = 0;
+    public static int boardWidth = 40;
+
+    MoveModel(int numberOfCars, int numberOfWaves, int wavesTime, boolean reverse, int MinTimeReaction,
+            int MaxTimeReaction, int trafficLightsTime) {
+        this.numberOfWaves = numberOfWaves;
+        this.wavesTime = wavesTime;
+        this.numberOfCars = numberOfCars;
+        this.MinTimeReaction = MinTimeReaction;
+        this.MaxTimeReaction = MaxTimeReaction;
+        this.trafficLightsTime = trafficLightsTime;
+        this.reverse = reverse;
+        this.trafficLight = new TrafficLight("green");
+        this.board = new Car[boardWidth];
+        this.AllCars = new ArrayList<>();
+        this.listOfCars = new ArrayList<>();
+    }
+
+    public TrafficLight getLight() {
+        return this.trafficLight;
+    }
+
+    public ArrayList<Car> move() {
+        newWaveOfCars();
+
+        listOfCars.clear();
+        trafficLightsChange();
+
+        for (int i = boardWidth - 1; i >= 0; i--) {
+            if (board[i] != null) {
+                if (i == boardWidth - 1) {
+                    board[i] = null;
+                } else if (i == MoveModel.boardWidth / 2 - 5) {
+                    if (board[i + 1] == null && this.trafficLight.getState().equals("green")) {
+                        board[i].setIsMoving(true);
+                        if (board[i].getCurrentReaction() == 0) {
+                            board[i + 1] = board[i];
+                            board[i] = null;
+                        } else {
+                            board[i].currentReactionDecrease();
+                        }
+                    } else {
+                        board[i].setIsMoving(false);
+                    }
+                } else {
+                    if (board[i + 1] == null) {
+                        board[i].setIsMoving(true);
+                        if (board[i].getCurrentReaction() == 0) {
+                            board[i + 1] = board[i];
+                            board[i] = null;
+                        } else {
+                            board[i].currentReactionDecrease();
+                        }
+                    } else {
+                        board[i].setIsMoving(false);
+                    }
+                }
+            }
+        }
+
+        newCarOnBoard();
+
+        addCarFromBoardToList();
+
+        if (this.reverse) {
+            reverseCoordination();
+        }
+
+        // for (Car item : listOfCars) {
+        // System.out.print("car:");
+        // System.out.println(item.getPositionX());
+        // }
+        // System.out.println("");
+        timeStep++;
+        return this.listOfCars;
+    }
+
+    private void reverseCoordination() {
+        for (Car car : listOfCars) {
+            int x = car.getPositionX();
+            int y = positionYRightToLeft;
+            car.setPosition(boardWidth - x - 1, y);
+        }
+    }
+
+    private void newWaveOfCars() {
+        if (numberOfWaves > 0 && timeStep % wavesTime == 0) {
+            for (int i = 0; i < numberOfCars; i++) {
+                Car car = new Car();
+                Random random = new Random();
+                if (this.MaxTimeReaction > 0) {
+                    int randomReaction = random.nextInt(this.MaxTimeReaction) + this.MinTimeReaction; // reaction
+                                                                                                      // between min and
+                                                                                                      // max;
+                    car.setReaction(randomReaction);
+                }
+                this.AllCars.add(car);
+            }
+            this.numberOfWaves--;
+        }
+    }
+
+    private void trafficLightsChange() {
+        if (timeStep % trafficLightsTime == 0) {
+            if (this.trafficLight.getState().equals("green")) {
+                this.trafficLight.setState("red");
+            } else {
+                this.trafficLight.setState("green");
+            }
+        }
+    }
+
+    private void newCarOnBoard() {
+        if (AllCars.size() > 0 && board[0] == null) { // new car from list appear on road
+            board[0] = AllCars.get(0);
+            AllCars.remove(0);
+        }
+    }
+
+    private void addCarFromBoardToList() {
+        for (int i = 0; i < boardWidth; i++) {
+            if (board[i] != null) {
+                board[i].setPosition(i, positionYLeftToRight);
+                listOfCars.add(board[i]);
+            }
+        }
+    }
+
+    public boolean isEnd() {
+        if (numberOfWaves == 0 && AllCars.size() == 0 && listOfCars.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/Road.java
@@ -0,0 +1,114 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.Timer;
+
+public class Road extends JPanel {
+    private int carWidth = 15;
+    private int carHeight = 12;
+    private int carAllSpace = RootDisplay.boardScale;
+    private static final Color ROAD_COLOR = Color.DARK_GRAY;
+    private static final Color SQUARE_COLOR = Color.BLUE;
+    private static final Color MARKING_COLOR = Color.WHITE;
+    private static final Color RED_COLOR = Color.RED;
+    private static final Color GREEN_COLOR = Color.GREEN;
+
+    private List<Point> points;
+    private Timer timer;
+
+    private MoveModel firstModel;
+    private MoveModel secondModel;
+
+    public Road() {
+        points = new ArrayList<>();
+        firstModel = new MoveModel(20, 4, 140, false,1,3,9);
+        secondModel = new MoveModel(20,4,20, true, 1,3,12);
+
+        timer = new Timer(20, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updatePoints();
+                repaint();
+                if(firstModel.isEnd() && secondModel.isEnd()){
+                    timer.stop();
+                }
+            }
+        });
+        timer.start();
+    }
+
+    private void updatePoints() {
+        points.clear();
+        for (Car item : firstModel.move()) {
+            int x = item.getPositionX();
+            int y = item.getPositionY();
+            points.add(new Point(x, y));
+        }
+        for (Car item : secondModel.move()) {
+            int x = item.getPositionX();
+            int y = item.getPositionY();
+            points.add(new Point(x, y));
+        }
+        java.util.Iterator<Point> iterator = points.iterator();
+        while (iterator.hasNext()) {
+            Point point = iterator.next();
+
+            int x = point.x;
+            int y = point.y;
+
+
+            // Wrap around the panel edges
+            if (x > getWidth()) {
+                iterator.remove(); // Remove the point from the list
+            } else {
+                point.setLocation(x, y);
+            }
+        }
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Set the background color of the panel to green
+        setBackground(ROAD_COLOR);
+
+        // Draw the marks on the board
+        for (int i = 0; i < getWidth(); i += 30) {
+            g.setColor(MARKING_COLOR);
+            g.fillRect(i, 24, 20, 2);
+        }
+
+        // Draw the squares on the board
+        for (Point coordinate : points) {
+            int x = coordinate.x*carAllSpace;
+            int y = coordinate.y;
+
+            g.setColor(SQUARE_COLOR);
+            g.fillRect(x, y, carWidth, carHeight); // Adjust the size of the square as needed
+        }
+
+        //Draw traffic lights for first model
+        if (firstModel.getLight().getState() == "red") {
+            g.setColor(RED_COLOR);
+        } else {
+            g.setColor(GREEN_COLOR);
+        }
+        g.fillRect(MoveModel.boardWidth*20/2-50, 32, 15,15);
+        
+        //Draw traffic lights for second model
+        if (secondModel.getLight().getState() == "red") {
+            g.setColor(RED_COLOR);
+        } else {
+            g.setColor(GREEN_COLOR);
+        }
+        g.fillRect(MoveModel.boardWidth*20/2+25, 5, 15,15);
+    }
+
+}
