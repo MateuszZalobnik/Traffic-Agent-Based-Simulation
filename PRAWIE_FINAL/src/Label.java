fc18f2f6296c12b54bd272d3e95f860654c96927
xmavv <129995423+xmavv@users.noreply.github.com>
2023-05-25 17:00:43 +0200

    PRAWIE_FINAL

---
+++ PRAWIE_FINAL/src/Label.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Label {
+    private static final Color BLACK = new Color(0, 0, 0);
+    private static final Color WHITE = new Color(255, 255, 255);
+    public Label(JPanel panel, String text, int x, int y) {
+        JLabel name_slider = new JLabel(text);
+        name_slider.setBounds(x, y, 200, 50);
+        panel.add(name_slider);
+        name_slider.setForeground(WHITE);
+        Font Calibri = new Font("Calibri", Font.BOLD, 20);
+        name_slider.setFont(Calibri);
+    }
+}
---
+++ PRAWIE_FINAL/src/TrafficLight.java
@@ -0,0 +1,27 @@
+public class TrafficLight {
+    private int state;
+    // state = 0 oś = x prosto i prawo dla osi x
+    // state = 1 oś = x lewo
+    // state = 2 oś = y prosto i prawo dla osi y
+    // state = 3 oś = y lewo
+
+    public TrafficLight(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public void changeLight(){
+        if(this.state == 3){
+            this.state= 0;
+        } else {
+            this.state++;
+        }
+    }
+}
