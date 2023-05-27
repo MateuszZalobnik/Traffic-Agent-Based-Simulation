80e7fb6e55442b5a8198cf32678afa321bb97cbe
Vaxjo <matiz12345678901@wp.pl>
2023-05-27 11:56:45 +0200

    ready

--- src/Road.java
+++ src/Road.java
@@ -1,40 +1,37 @@
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.List;
 import javax.swing.Timer;
 
 public class Road extends JPanel {
     private int carWidth = 15;
-    private int carHeight = 12;
+    private int carHeight = 15;
     private int carAllSpace = RootDisplay.boardScale;
     private static final Color ROAD_COLOR = Color.DARK_GRAY;
-    private static final Color SQUARE_COLOR = Color.BLUE;
+    private static final Color STRAIGHT_COLOR = Color.BLUE;
+    private static final Color LEFT_COLOR = Color.PINK;
+    private static final Color RIGHT_COLOR = Color.YELLOW;
     private static final Color MARKING_COLOR = Color.WHITE;
     private static final Color RED_COLOR = Color.RED;
     private static final Color GREEN_COLOR = Color.GREEN;
 
-    private List<Point> points;
+    private ArrayList<Car> listOfCars = new ArrayList<>(); // cars on board
+
     private Timer timer;
 
-    private MoveModel firstModel;
-    private MoveModel secondModel;
+    private CrossRoadModel firstModel;
 
-    public Road() {
-        points = new ArrayList<>();
-        firstModel = new MoveModel(20, 4, 20, false,1,3,9);
-        secondModel = new MoveModel(20,4,20, true, 3,6,9);
-
+    public Road(int number_of_cars, int maxTimeReaction, int minTimeReaction, int numberOfWaves, int wavesTime, int lightsTime) {
+        firstModel = new CrossRoadModel(number_of_cars, numberOfWaves, wavesTime,minTimeReaction,maxTimeReaction,lightsTime);
         timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 updatePoints();
                 repaint();
-                if(firstModel.isEnd() && secondModel.isEnd()){
+                if(firstModel.isEnd()){
                     timer.stop();
                 }
             }
@@ -43,33 +40,37 @@ public class Road extends JPanel {
     }
 
     private void updatePoints() {
-        points.clear();
-        for (Car item : firstModel.move()) {
-            int x = item.getPositionX();
-            int y = item.getPositionY();
-            points.add(new Point(x, y));
-        }
-        for (Car item : secondModel.move()) {
-            int x = item.getPositionX();
-            int y = item.getPositionY();
-            points.add(new Point(x, y));
-        }
-        java.util.Iterator<Point> iterator = points.iterator();
+        listOfCars.clear();
+        listOfCars = firstModel.move();
+        java.util.Iterator<Car> iterator = listOfCars.iterator();
         while (iterator.hasNext()) {
-            Point point = iterator.next();
+            Car car = iterator.next();
 
-            int x = point.x;
-            int y = point.y;
-
+            int x = car.getPositionX();
+            int y = car.getPositionY();
 
             // Wrap around the panel edges
             if (x > getWidth()) {
                 iterator.remove(); // Remove the point from the list
             } else {
-                point.setLocation(x, y);
+                car.setPosition(x, y);
             }
         }
+    }
 
+    private void DrawTrafficLight(Graphics g, int x, int y) {
+        // Draw traffic lights
+        int pointSize = 20;
+        // g.setColor(Color.RED);
+        g.fillOval(x - pointSize / 2, y - pointSize / 2, pointSize, pointSize);
+
+        // Add the letter "L" next to the point
+        // String letter = "L";
+        // int letterOffsetX = -3;
+        // int letterOffsetY = 5;
+        // g.setColor(Color.BLACK);
+        // g.drawString(letter, x + letterOffsetX, y + letterOffsetY);
+        g.fillOval(x - pointSize / 2, y - pointSize / 2, pointSize, pointSize);
     }
 
     @Override
@@ -82,33 +83,56 @@ public class Road extends JPanel {
         // Draw the marks on the board
         for (int i = 0; i < getWidth(); i += 30) {
             g.setColor(MARKING_COLOR);
-            g.fillRect(i, 24, 20, 2);
+            g.fillRect(i, 398, 20, 2);
         }
-
+        for (int i = 0; i < getWidth(); i += 30) {
+            g.setColor(MARKING_COLOR);
+            g.fillRect(398, i, 2, 20);
+        }
         // Draw the squares on the board
-        for (Point coordinate : points) {
-            int x = coordinate.x*carAllSpace;
-            int y = coordinate.y;
+        for (Car coordinate : listOfCars) {
+            int x = coordinate.getPositionX() * carAllSpace;
+            int y = coordinate.getPositionY();
 
-            g.setColor(SQUARE_COLOR);
+            String dest = coordinate.getDestination();
+            switch (dest) {
+                case "left":
+                    g.setColor(LEFT_COLOR);
+                    break;
+                case "right":
+                    g.setColor(RIGHT_COLOR);
+                    break;
+                default:
+                    g.setColor(STRAIGHT_COLOR);
+                    break;
+            }
             g.fillRect(x, y, carWidth, carHeight); // Adjust the size of the square as needed
         }
 
-        //Draw traffic lights for first model
-        if (firstModel.getLight().getState() == "red") {
-            g.setColor(RED_COLOR);
-        } else {
-            g.setColor(GREEN_COLOR);
+        g.setColor(RED_COLOR);
+        DrawTrafficLight(g, 370, 430);
+        DrawTrafficLight(g, 430, 370);
+        DrawTrafficLight(g, 350, 430);
+        DrawTrafficLight(g, 450, 370);
+        DrawTrafficLight(g, 370, 370);
+        DrawTrafficLight(g, 370, 350);
+        DrawTrafficLight(g, 430, 430);
+        DrawTrafficLight(g, 430, 450);
+        g.setColor(GREEN_COLOR);
+
+        if (firstModel.getTrafficLight() == 0) {
+            DrawTrafficLight(g, 370, 430);
+            DrawTrafficLight(g, 430, 370);
+        } else if (firstModel.getTrafficLight() == 1) {
+            DrawTrafficLight(g, 350, 430);
+            DrawTrafficLight(g, 450, 370);
+        } else if (firstModel.getTrafficLight() == 2) {
+            DrawTrafficLight(g, 370, 370);
+            DrawTrafficLight(g, 430, 430);
+        } else if (firstModel.getTrafficLight() == 3) {
+            DrawTrafficLight(g, 370, 350);
+            DrawTrafficLight(g, 430, 450);
         }
-        g.fillRect(MoveModel.boardWidth*20/2-50, 32, 15,15);
-        
-        //Draw traffic lights for second model
-        if (secondModel.getLight().getState() == "red") {
-            g.setColor(RED_COLOR);
-        } else {
-            g.setColor(GREEN_COLOR);
-        }
-        g.fillRect(MoveModel.boardWidth*20/2+25, 5, 15,15);
     }
 
 }
--- src/RootDisplay.java
+++ src/RootDisplay.java
@@ -1,27 +1,92 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public class RootDisplay {
     public static int boardScale = 20;
-    public static void main(String[] args) {
+    private static final Color BLUE = new Color(0, 50, 100);
+    private static final Color LIGHT_BLUE = new Color (0, 100, 180);
+    private static final Color BLACK = new Color (0, 0, 0);
+    private static final Color WHITE = new Color(255, 255, 255);
+
+    public static void display() {
 
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Traffic Agent Based Model");
-            frame.setSize(MoveModel.boardWidth*boardScale+400, MoveModel.boardWidth*boardScale);
+            frame.setSize(CrossRoadModel.boardWidth * boardScale + 400, CrossRoadModel.boardWidth * boardScale);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
             JPanel greenBoard = new JPanel(null);
-            greenBoard.setBackground(Color.GREEN);
-            greenBoard.setBounds(400, 0, MoveModel.boardWidth*boardScale, MoveModel.boardWidth*boardScale);
-            Road road = new Road();
-            road.setBounds(0, (MoveModel.boardWidth*boardScale/2)-25, MoveModel.boardWidth*boardScale, 50);
+            greenBoard.setBackground(BLUE);
+            greenBoard.setBounds(400, 0, CrossRoadModel.boardWidth * boardScale,
+                    CrossRoadModel.boardWidth * boardScale);
+
+            Road road = new Road(5, 3, 3, 3, 80, 7);
+            road.setBounds(0, 0, CrossRoadModel.boardWidth * boardScale, CrossRoadModel.boardWidth * boardScale);
             greenBoard.add(road);
             frame.add(greenBoard);
 
-            frame.setLayout(null);
+//            icon
+            Image icon = Toolkit.getDefaultToolkit().getImage("TrafficLights.png");
+            frame.setIconImage(icon);
+
+            Label label = new Label(greenBoard, "Liczba samochodów", 850, 10);
+            Label label2 = new Label(greenBoard, "Max czas reakcji", 850, 110);
+            Label label3 = new Label(greenBoard, "Min czas reakcji", 850, 210);
+            Label label4 = new Label(greenBoard, "Liczba fal samochodów", 850, 310);
+            Label label5 = new Label(greenBoard, "Czas fal", 850, 410);
+            Label label6 = new Label(greenBoard, "Czas swiatel", 850, 510);
+
+            Slider slider_number_of_cars = new Slider(850, 50, 0, 10, 5,2,1);
+            Slider slider_max_time_reaction = new Slider(850, 150, 1, 5, 3,2,1);
+            Slider slider_min_time_reaction = new Slider(850, 250, 1, 5, 3,2 ,1);
+            Slider slider_number_of_waves = new Slider(850, 350, 1, 5, 3,2,1);
+            Slider slider_waves_time = new Slider(850, 450, 20, 140, 80,10,20);
+            Slider slider_lights_time = new Slider(850, 550, 1,13, 7,2,1);
+
+//            print sliders
+            slider_number_of_cars.addPanel(greenBoard);
+            slider_max_time_reaction.addPanel(greenBoard);
+            slider_min_time_reaction.addPanel(greenBoard);
+            slider_number_of_waves.addPanel(greenBoard);
+            slider_waves_time.addPanel(greenBoard);
+            slider_lights_time.addPanel(greenBoard);
+
+            JButton button = new JButton("Restart");
+            button.setBounds(876, 645, 200, 70);
+            button.setBackground(LIGHT_BLUE);
+            Font Calibri = new Font("Calibri", Font.CENTER_BASELINE, 35);
+            button.setFont(Calibri);
+            button.setForeground(WHITE);
+            greenBoard.add(button);
+
+            button.addActionListener(e ->
+            {
+                int slider_number_of_carsValue = slider_number_of_cars.getValue();
+                int slider_max_time_reactionValue = slider_max_time_reaction.getValue();
+                int slider_min_time_reactionValue = slider_min_time_reaction.getValue();
+                int slider_number_of_wavesValue = slider_number_of_waves.getValue();
+                int slider_waves_timeValue = slider_waves_time.getValue();
+                int slider_lights_timeValue = slider_lights_time.getValue();
+                greenBoard.removeAll();
+                Road roadToRender = new Road(slider_number_of_carsValue, slider_max_time_reactionValue, slider_min_time_reactionValue, slider_number_of_wavesValue, slider_waves_timeValue, slider_lights_timeValue);
+                roadToRender.setBounds(0, 0, CrossRoadModel.boardWidth * boardScale, CrossRoadModel.boardWidth * boardScale);
+                greenBoard.add(roadToRender);
+                greenBoard.add(button);
+
+                slider_number_of_cars.addPanel(greenBoard);
+                slider_max_time_reaction.addPanel(greenBoard);
+                slider_min_time_reaction.addPanel(greenBoard);
+                slider_number_of_waves.addPanel(greenBoard);
+                slider_waves_time.addPanel(greenBoard);
+                slider_lights_time.addPanel(greenBoard);
+//                frame.repaint();
+            });
+
+            greenBoard.setSize(1000, 1000);
+
+//            frame.setLayout(null);
             frame.setVisible(true);
+
         });
     }
 }
